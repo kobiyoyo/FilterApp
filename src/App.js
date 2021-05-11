2ee435a29537c719989cd85a135f49a72402cce8
Kobiyoyo <adamachubi@gmail.com>
2021-05-11 23:35:55 +0100

    add rsuite table

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import React, { useEffect, useState } from "react";
 import axios from 'axios';
 import DataTable from './components/DataTable';
+import 'rsuite/dist/styles/rsuite-default.css';
+import classes from './styles/App.module.css';
 
 const App = () => {
   const [data, setData] = useState([]);
-
+  const [search,setSearch] = useState("");
   useEffect(() => {
       getUser()
     },
-    [data]);
+    []);
     const getUser = async () => {
       try{
          const response = await axios.get('https://jsonplaceholder.typicode.com/users')
@@ -20,15 +22,25 @@ const App = () => {
       }
      }
 
-  console.log(data);
+  const setInput = (setter) => (event) =>{
+    setter(event.currentTarget.value)
+  }
+  const filteredData = (items) => {
+    return items.filter((item)=> 
+    search === "" ||
+    item.name.toLowerCase().includes(search.toLowerCase()) ||
+    item.username.toLowerCase().includes(search.toLowerCase()) ||
+    item.email.toLowerCase().includes(search.toLowerCase()) 
+    )
+  }
+  console.log(filteredData(data));
   return (
    
-    <div className="App">
-       <DataTable data={data} />
-      <h1>Sweetie</h1>
-      {data.map((user)=>(
-        <h1>{user.name}</h1>
-      ))}
+    <div>
+      <div className={classes.InputWrapper}>
+      <input placeholder="Search" onChange={setInput(setSearch)} value={search} />
+      </div>
+       <DataTable data={filteredData(data)} />
     </div>
   );
 };
--- src/components/DataTable/index.js
+++ src/components/DataTable/index.js
@@ -1,26 +1,60 @@
 import React from "react";
+import { Table } from 'rsuite';
 
 const DataTable = ({ data }) => {
-  const columns = data[0] && Object.keys(data[0]);
+  const { Column, HeaderCell, Cell, Pagination } = Table;
   return (
-    <table cellPadding={0} cellSpacing={0}>
-      <thead>
-        <tr>{data[0] && columns.map((heading) => <th>{heading}</th>)}</tr>
-      </thead>
-      <tbody>
-        {data.map((row) => (
-          <tr>
-            <td>{row.id}</td>
-            <td>{row.name}</td>
-            <td>{row.username}</td>
-            <td>{row.email}</td>
-            <td>{row.address.street }</td>
-            <td>{row.phone}</td>
-          </tr>
-        ))}
-      </tbody>
-    </table>
-  );
+    <div>
+    <Table
+      virtualized
+      height={400}
+      data={data}
+      onRowClick={data => {
+        console.log(data);
+      }}
+    >
+      <Column width={70} align="center" fixed>
+        <HeaderCell>Id</HeaderCell>
+        <Cell dataKey="id" />
+      </Column>
+
+      <Column width={130}>
+        <HeaderCell>First Name</HeaderCell>
+        <Cell dataKey="name" />
+      </Column>
+
+      <Column width={130}>
+        <HeaderCell>Username</HeaderCell>
+        <Cell dataKey="username" />
+      </Column>
+
+      <Column width={200}>
+        <HeaderCell>Street</HeaderCell>
+        <Cell>{rowData =>  rowData.address.street}</Cell>
+      </Column>
+      
+      <Column width={200}>
+        <HeaderCell>City</HeaderCell>
+        <Cell>{rowData =>  rowData.address.city}</Cell>
+      </Column>
+
+      <Column width={200}>
+        <HeaderCell>Website</HeaderCell>
+        <Cell dataKey="website" />
+      </Column>
+
+      <Column width={200}>
+        <HeaderCell>Phone</HeaderCell>
+        <Cell dataKey="phone" />
+      </Column>
+
+      <Column width={200}>
+        <HeaderCell>Company Name</HeaderCell>
+        <Cell>{rowData =>  rowData.company.name}</Cell>
+      </Column>
+    </Table>
+  </div>
+  )
 };
 
 export default DataTable;
